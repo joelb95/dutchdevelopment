c911d6429c3867c29dfd8c563b9ebf1ae58991cc
ritchiebos <richard.bos93@gmail.com>
2016-05-20 09:59:28 +0200

    Chg CSS + Project

--- incl/content/project.php
+++ incl/content/project.php
@@ -3,11 +3,11 @@
   <button class="cn-button cnmenu" id="cn-button">+</button>
   <div class="cn-wrapper cnmenu" id="cn-wrapper">
     <ul class="cnmenu">
-      <li class="cnmenu"><a href="#"><span class="fa fa-home"></span></a></li>
-      <li class="cnmenu"><a href="#"><span class="fa fa-home"></span></a></li>
-      <li class="cnmenu"><a href="#"><span class="fa fa-home"></span></a></li>
-      <li class="cnmenu"><a href="#"><span class="fa fa-home"></span></a></li>
-      <li class="cnmenu"><a href="#"><span class="fa fa-home"></span></a></li>
+      <li class="cnmenu"><a href="#"><i class="fa fa-home" aria-hidden="true"></i></a></li>
+      <li class="cnmenu"><a href="#"><i class="fa fa-book" aria-hidden="true"></i></a></li>
+      <li class="cnmenu"><a href="#"><i class="fa fa-video-camera" aria-hidden="true"></i></a></li>
+      <li class="cnmenu"><a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></li>
+      <li class="cnmenu"><a href="#"><i class="fa fa-cog" aria-hidden="true"></i></a></li>
     </ul>
   </div>
   <div id="cn-overlay" class="cn-overlay cnmenu"></div>
